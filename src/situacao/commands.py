dbf96f8a17906ca38d34003a3c364cddd8c93c5d
Joao Figueiredo <joao@hotjar.com>
2020-05-24 00:02:35 +0100

    moar ux.

--- src/situacao/commands.py
+++ src/situacao/commands.py
@@ -1,3 +1,5 @@
+from yaspin import yaspin
+
 from . import minsaude
 from . import parse
 from . import pdf
@@ -6,18 +8,27 @@ from . import util
 
 
 def command_concelho(args):
+    sp = yaspin()
+    sp.color = 'blue'
+    sp.start()
+    sp.text = "checking min-saude.pt..."
     reports = minsaude.crawl()
-    print(f"found {len(reports)} interesting reports.")
+    sp.write(f"> found {len(reports)} interesting reports.")
+    sp.text = "downloading missing reports..."
     fetched = minsaude.maybe_fetch(reports)
     if fetched:
-        print(f"fetched {fetched} missing reports.")
+        sp.write(f"> fetched {fetched} missing reports.")
+    sp.text = "parsing PDF reports..."
     text = pdf.extract_text(reports)
     dataset = parse.from_text(text)
     m = args.name
     if m not in dataset:
+        sp.red.fail("✘")
         util.abort(f"unknown municipality: {m}. tip: run `situacao concelho --list` for a full list of available municipalities.")
     for d, c in dataset[m].items():
-        print(f"{d.isoformat()}: {c}")
+        sp.write(f"{d.isoformat()}: {c}")
+    sp.text = 'success!'
+    sp.green.ok("✔")
 
 
 def add_concelho(sub_parser):
